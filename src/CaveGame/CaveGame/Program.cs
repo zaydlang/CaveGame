4d01fcfe7ab1c61ba2619ac6ba502ecc94a59d52
0xFireball <0xFireball@outlook.com>
2018-10-21 14:36:59 -0700

    create project

---
+++ src/CaveGame/CaveGame/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CaveGame {
+    public static class Program {
+        [STAThread]
+        static void Main() {
+            using (var game = new NGame())
+                game.Run();
+        }
+    }
+}
