656bc8c5d4b07fcfd1aba1ac16cf220cc81e6880
zaydqumsieh <zayd@qumsieh.net>
2018-11-29 08:58:35 -0800

    better jumping

--- src/CaveGame/CaveGame/Components/Player/PlayerComponent.cs
+++ src/CaveGame/CaveGame/Components/Player/PlayerComponent.cs
@@ -38,7 +38,8 @@ namespace CaveGame.Components {
                 velocity.X = 0;
             }
             if (Input.isKeyDown(Keys.Up) && grounded) {
-                Console.WriteLine("jump");
+                Console.WriteLine(grounded);
+                grounded = false;
                 velocity.Y = -(float) Math.Sqrt(2 * Constants.GRAVITY * Constants.PLAYER_JUMP_HEIGHT);
             }
 
@@ -56,8 +57,8 @@ namespace CaveGame.Components {
             for (int i = 0; i < waterTiles.Count; i++) {
                 TiledTile tile = waterTiles[i];
 
-                Console.WriteLine(underWater + " " + i + " " + waterTiles[i].id);
-                Console.WriteLine(waterTiles.Count);
+                // Console.WriteLine(underWater + " " + i + " " + waterTiles[i].id);
+                // Console.WriteLine(waterTiles.Count);
                 if (tile.id == (int) Constants.Id.Water) {
                     velocity.Y -= Constants.BOUYANT_FORCE * Time.deltaTime;
                     
@@ -83,9 +84,15 @@ namespace CaveGame.Components {
             if (collider.collidesWithAnyMultiple(motion, collisions)) {
                 for (int i = 0; i < collisions.Count; i++) {
                     motion -= collisions[i].minimumTranslationVector;
-                    Console.WriteLine(entity.position);
-                    if (Math.Abs(collisions[i].normal.Y) == 1) {
+                    // Console.WriteLine(collisions[i].minimumTranslationVector);
+                    // Console.WriteLine(entity.position);
+                    if (collisions[i].normal.Y == 1) {
                         velocity.Y = 0;
+                    }
+
+                    if (collisions[i].normal.Y == -1) {
+                        velocity.Y = 0;
+                        Console.WriteLine("Hit floor!");
                         grounded = true;
                     }
                 }
